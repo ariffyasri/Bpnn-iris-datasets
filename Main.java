de813f40cfa3031ee4938f16ea4a8e64fbf46e92
aiscool <ariffacm@gmail.com>
2016-04-30 21:33:57 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ public class Main {
    public static void main(String [] args) {
       Backpropogation bp = new Backpropogation();
       bp.inputByFile("iris - normalize1.csv",0.5,5000);
-      //bp.bpnntraining();
+      bp.bpnntraining();
       double numberOfClasses = 3.0;
-      bp.bpnntesting("testing.txt","weightraining.txt",numberOfClasses);
+      //bp.bpnntesting("testing.txt","weightraining.txt",numberOfClasses);
    }
 }
